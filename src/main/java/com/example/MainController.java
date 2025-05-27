dbedfa39881f991c17d3dc4ec4e73bf8687db77d
Sallai András <andrewteki@gmail.com>
2025-05-27 10:36:57 +0200

    Az initialize metódus kész

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.time.LocalDate;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -8,6 +10,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.StringConverter;
 
 public class MainController {
 
@@ -15,10 +19,10 @@ public class MainController {
     private CheckBox absoluteCheck;
 
     @FXML
-    private TableColumn<?, ?> absoluteCol;
+    private TableColumn<Racer, Boolean> absoluteCol;
 
     @FXML
-    private TableColumn<?, ?> birthCol;
+    private TableColumn<Racer, LocalDate> birthCol;
 
     @FXML
     private DatePicker birthPicker;
@@ -27,22 +31,49 @@ public class MainController {
     private ToggleGroup category;
 
     @FXML
-    private TableColumn<?, ?> categoryCol;
+    private TableColumn<Racer, String> categoryCol;
 
     @FXML
-    private TableColumn<?, ?> idCol;
+    private TableColumn<Racer, Integer> idCol;
 
     @FXML
     private TextField idField;
 
     @FXML
-    private TableColumn<?, ?> nameCol;
+    private TableColumn<Racer, String> nameCol;
 
     @FXML
     private TextField nameField;
 
     @FXML
-    private TableView<?> racerTable;
+    private TableView<Racer> racerTable;
+
+    @FXML
+    void initialize() {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        birthCol.setCellValueFactory(new PropertyValueFactory<>("birth"));
+        categoryCol.setCellValueFactory(new PropertyValueFactory<>("category"));
+        absoluteCol.setCellValueFactory(new PropertyValueFactory<>("absolute"));
+        StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
+            @Override
+            public String toString(LocalDate date) {
+                if (date == null) {
+                    return "";
+                }
+                return date.toString();
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string == null || string.trim().length() == 0) {
+                    return null;
+                }
+                return LocalDate.parse(string);
+            }
+        };
+        birthPicker.setConverter(converter);
+    }
 
     @FXML
     void onClickAboutButton(ActionEvent event) {
